fd7cbd6194e9519ec47b608713386a16ebabb1c9
azarashin <azarashin1979@gmail.com>
2021-10-08 08:54:05 +0900

    テストケースを追加(Player クラスのテスト)。

--- Assets/UnityTetris/Tests/UnitTestPlayer.cs
+++ Assets/UnityTetris/Tests/UnitTestPlayer.cs
@@ -4,22 +4,38 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityTetris;
+using UnityTetris.Abstract;
+using UnityTetris.Interface;
 
 public class UnitTestPlayer
 {
     [Test]
     public void UnitTest001()
     {
-        Player p = NewPlayer(); 
+        int fallLevel = 4; 
+        Player p = NewPlayer();
+        ISoundManager sound = new StubSoundManager();
+        StubBlockSet[] bs = new StubBlockSet[] { StubBlockSet() };
+        StubField field = StubFieldPrefab();
+        p.Setup(field, bs, sound, fallLevel);
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator UnitTestPlayerWithEnumeratorPasses()
+    public IEnumerator UnityTest001()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
+        ObjectPicker<StubField> fieldPicker = new ObjectPicker<StubField>(); 
+        int fallLevel = 4;
+        Player p = NewPlayer();
+        ISoundManager sound = new StubSoundManager();
+        StubBlockSet[] bs = new StubBlockSet[] { StubBlockSet() };
+        StubField fieldPrefab = StubFieldPrefab();
+        fieldPicker.Pick(); 
+        p.Setup(fieldPrefab, bs, sound, fallLevel);
+        StubField[] fields = fieldPicker.Pick();
+        Assert.AreEqual(1, fields.Length);
+        Assert.AreEqual("ResetField(-1,-1,-1)\n", fields[0].CallList); 
         yield return null;
     }
 
@@ -29,6 +45,24 @@ public class UnitTestPlayer
         GameObject obj = GameObject.Instantiate(prefab);
 
         Player ret = obj.GetComponent<Player>();
+        return ret;
+
+    }
+
+    private StubBlockSet StubBlockSet()
+    {
+        GameObject prefab = Resources.Load<GameObject>("UnityTetris/Prefabs/Test/StubBlockSet");
+        GameObject obj = GameObject.Instantiate(prefab);
+
+        StubBlockSet ret = obj.GetComponent<StubBlockSet>();
+        return ret;
+
+    }
+
+    private StubField StubFieldPrefab()
+    {
+        GameObject prefab = Resources.Load<GameObject>("UnityTetris/Prefabs/Test/StubField");
+        StubField ret = prefab.GetComponent<StubField>();
         return ret;
 
     }
