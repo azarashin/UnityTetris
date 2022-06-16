388084e10e73032a78a708cdd0459972c516928c
azarashin <azarashin1979@gmail.com>
2022-06-17 08:31:58 +0900

    GitHub Action を全体的に見直し。

--- python/index_generator.py
+++ python/index_generator.py
@@ -141,10 +141,11 @@ def generate_mindmap_page(input_path, svgs_path):
     							
 
 def generate_svg_page(input_path, svgs_path):
-	path_pattern = re.compile("^.*/([^/]+)/([^/]+).(pu|puml)$")
+	path_pattern = re.compile("^(.+)/([^/]+).(pu|puml)$")
 	title_pattern = re.compile("^\s*title\s*(.*)$")
 	pus = [d.replace('\\', '/') for d in glob.glob(input_path + '/**/*.pu', recursive=True)]
 	pus.extend([d.replace('\\', '/') for d in glob.glob(input_path + '/**/*.puml', recursive=True)])
+	pus = [d[len(input_path):] for d in pus]
 	dic={}
 	for pu in pus:
 		pu=pu.replace('\\', '/')
